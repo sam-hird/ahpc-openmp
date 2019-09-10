3525579b8ed2389a37e511a01cfd28fa305fcc37
Samuel Hird <sh15793@newblue4.cm.cluster>
2019-09-10 11:04:12 +0100

    SAFE broke

--- env.sh
+++ env.sh
@@ -1,2 +1,9 @@
 # Add any `module load` or `export` commands that your code needs to
 # compile and run to this file.
+module add shared default-environment
+
+module load languages/intel-compiler-16-u2
+
+module add tools/git-2.18.0
+module load cuda/toolkit/7.5.18
+module load languages/python-2.7.6
